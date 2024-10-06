0828c7c4f5a938e85f671c35487018dbbee25c79
jawade.arpeet <jawade.arpeet@gmail.com>
2024-10-07 01:58:05 +0530

    server init

---
+++ server/index.ts
@@ -0,0 +1,26 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGODB_URI as string)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+const app = express();
+
+app.use(cors());
+app.use(cookieParser());
+app.use(express.json());
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server is running on port ${process.env.PORT}`);
+});
