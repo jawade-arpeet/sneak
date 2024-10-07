70e2a91c83baefcdb18ae41b521918c278d282d5
jawade.arpeet <jawade.arpeet@gmail.com>
2024-10-07 19:28:09 +0530

    add sneaker model

---
+++ server/models/sneaker.ts
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+const sneakerModel = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  brand: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+});
+
+const Sneaker = mongoose.model("Sneaker", sneakerModel);
